f6750e07afa137d1750a9d6fd6b1b294aeb0484a
Ric Mershon <rmersh@ricmershon.com>
2020-04-23 13:35:31 -0400

    Added mail.php

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 $errors = '';
-$myemail = 'ceo@arthurbernierjr.com';//<-----Put Your email address here.
+$myemail = 'ric.mershon@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']))
@@ -25,6 +25,7 @@ if( empty($errors))
 	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message Phone \n $phone";
 	$headers = "From: $myemail\n";
 	$headers .= "Reply-To: $email_address";
+  // echo 'console.log('Sending email')';
 	mail($to,$email_subject,$email_body,$headers);
 	//redirect to the 'thank you' page
 	header('Location: https://ricmershon.github.io');
