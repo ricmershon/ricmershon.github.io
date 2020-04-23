3d8820b9eb3dcda87afae7110657e472b3db9f96
Ric Mershon <rmersh@ricmershon.com>
2020-04-23 15:02:07 -0400

    Added mail.php invocation

--- mail.php
+++ mail.php
@@ -22,11 +22,11 @@ if( empty($errors))
 	$to = $myemail;
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message Phone \n $phone";
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message Phone \n ";
 	$headers = "From: $myemail\n";
 	$headers .= "Reply-To: $email_address";
 	mail($to,$email_subject,$email_body,$headers);
 	//redirect to the 'thank you' page
-	header('Location: https://www.ricmershon.com');
+	header('Location: https://ricmershon-portfolio.herokuapp.com');
 }
 ?>
