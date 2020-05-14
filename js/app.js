8436708c8d271de232b084780bbf954270d83ef8
Ric Mershon <rmersh@ricmershon.com>
2020-05-13 23:07:37 -0400

    Before adding modal boxes.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,13 @@
 
 $(() => {
-    $('#send-message-button').click(() => {
-      for (var i = 0; i < 100; i++) {
-      console.log('submit clicked');
-
-      }
-    })
-    //
-    // $('.header-menu-item').click(() => {
-    //     getHeadlinesData($(event.currentTarget).attr('id'), '')
-    // })
-    // $('.sub-menu-item').click(() => {
-    //     getHeadlinesData('us', $(event.currentTarget).attr('id'))
-    // })
-    // $('#submenu-button').click(() => {
-    //     event.preventDefault();
-    //     getKeywordsData($('#submenu-keyword-box[type="text"]').val())
-    // })
-    // $('#welcome-button').click(() => {
-    //     event.preventDefault();
-    //     getKeywordsData($('#welcome-keyword-box[type="text"]').val())
-    // })
+  console.log("Hello world");
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
 })
